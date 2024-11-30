e0f211bb7f7521ea2f15cf5b7011b5f81b7d5d20
drozdovalena <drozdova.el.gen@gmail.com>
2024-11-30 23:09:47 +0100

    6 sprint, start 21 hw

--- src/features/user-accaunts/security/application/security.service.ts
+++ src/features/user-accaunts/security/application/security.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { SecuritySqlRepository } from '../infrastucture/security.sql.repository';
 import { SecurityTorRepository } from '../infrastucture/security.tor.repository';
 
 @Injectable()
 export class SecurityService {
-  constructor(
-    protected securitySqlRepository: SecuritySqlRepository,
-    protected securityTorRepository: SecurityTorRepository,
-  ) {}
+  constructor(protected securityTorRepository: SecurityTorRepository) {}
 
   async updateSession(id: string, iat: Date, exp: Date) {
     await this.securityTorRepository.updateSession(id, iat, exp);
